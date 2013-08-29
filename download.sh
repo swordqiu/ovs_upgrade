d994e838812f658dbded1ea26ff2a723aa69afc9
Qiu Jian <qiujian@meituan.com>
2013-08-29 15:02:02 +0800

    Add patches for 1.11.0

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
-wget http://openvswitch.org/releases/openvswitch-1.4.0.tar.gz
 wget http://openvswitch.org/releases/openvswitch-1.4.6.tar.gz
 wget http://openvswitch.org/releases/openvswitch-1.7.3.tar.gz
 wget http://openvswitch.org/releases/openvswitch-1.9.0.tar.gz
 wget http://openvswitch.org/releases/openvswitch-1.10.0.tar.gz
+wget http://openvswitch.org/releases/openvswitch-1.11.0.tar.gz
